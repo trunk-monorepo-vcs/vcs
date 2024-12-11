c871763ac8e641f36df703ca384c0f8988653910
A-GigaC <nporozenko@gmail.com>
2024-12-11 20:25:39 +0700

    fix response recieving

--- client/src/client.c
+++ client/src/client.c
@@ -10,6 +10,9 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#define PACKAGE_SIZE 1024
+
+
 int connectToServer(const char *ip, const int port) {
     int connection = socket(AF_INET, SOCK_STREAM, 6);
     if (connection == -1) {
@@ -46,24 +49,36 @@ char *sendReqAndHandleResp(const int connection, const char *dataToSend, const i
         return NULL;
     }
 
-    // Recieve data
-    int64_t packageSize;
-    if (recv(connection, &packageSize, 8, 0) != 8) {
-        perror("Troubles in packSize reading");
-        return NULL;
-    }
-
+    // // Recieve data
+    // int64_t packageSize;
+    // if (recv(connection, &packageSize, 8, 0) != 8) {
+    //     perror("Troubles in packSize reading");
+    //     return NULL;
+    // }
+    uint64_t packageSize = PACKAGE_SIZE;
+    uint64_t readedBytes, lastIndex = 0;
     char *recvBuffer = malloc(packageSize);
     if (recvBuffer == NULL) {
         perror("NULL pointer");
         return NULL;
     }
+    do {
+        readedBytes = recv(connection, (char *)((uint64_t)recvBuffer + lastIndex), packageSize, 0);
+        lastIndex += readedBytes;
+        if (readedBytes == packageSize) {
+            recvBuffer = realloc(recvBuffer, packageSize * 2);
+            if (recvBuffer == NULL) {
+                perror("NULL pointer");
+                return NULL;
+            }
+        }
+    } while (readedBytes);
 
-    if (recv(connection, recvBuffer, packageSize, 0) < packageSize) {
-        perror("Cannot recv to buffer");
-        free(recvBuffer);
-        return NULL;
-    }
+    // if (recv(connection, recvBuffer, packageSize, 0) < packageSize) {
+    //     perror("Cannot recv to buffer");
+    //     free(recvBuffer);
+    //     return NULL;
+    // }
 
     return recvBuffer;
 }
