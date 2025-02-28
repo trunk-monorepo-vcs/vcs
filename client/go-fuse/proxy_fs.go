e01385c77d505d17fff5974ff583b80ec4e773a1
PavelBun <144594950+PavelBun@users.noreply.github.com>
2025-03-01 01:55:05 +0700

    added sqlite

--- client/go-fuse/proxy_fs.go
+++ client/go-fuse/proxy_fs.go
@@ -2,37 +2,74 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// This program is the analogon of libfuse's hello.c, a a program that
+// This program is the analogon of libfuse's hello.c, a program that
 // exposes a single file "file.txt" in the root directory.
+
 package main
 
 import (
 	"context"
+	"database/sql"
 	"flag"
 	"log"
+	
 	"syscall"
 
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/hanwen/go-fuse/v2/fs"
 	"github.com/hanwen/go-fuse/v2/fuse"
 )
 
-type HelloRoot struct {
+type proxyFs struct {
 	fs.Inode
+	db     *sql.DB
+	dbPath string
 }
-func (r *HelloRoot) logToFile(message string) syscall.Errno{
-	node := &r.Inode
-	for !node.IsRoot() {
-		_, node = node.Parent()
+
+type SQLiteFile struct {
+	fs.Inode
+	db   *sql.DB
+	name string
+}
+
+func (r *proxyFs) logToFile(message string) {
+	ch := r.GetChild("log.txt")
+	if ch == nil {
+		return
 	}
-    ch := node.GetChild("log.txt")
 
-	file, _ := ch.Operations().(*fs.MemRegularFile)
-	
-	file.Data  = append(file.Data, []byte(message+"\n")...)
-	return 0;
+	file, ok := ch.Operations().(*fs.MemRegularFile)
+	if !ok {
+		return
+	}
 
+	file.Data = append(file.Data, []byte(message+"\n")...)
 }
-func (r *HelloRoot) OnAdd(ctx context.Context) {
+
+func initDB(db *sql.DB) {
+	_, err := db.Exec(`
+		CREATE TABLE IF NOT EXISTS files(
+			name TEXT PRIMARY KEY,
+			data BLOB,
+			mode INTEGER
+		)
+	`)
+	if err != nil {
+		log.Println("Failed to create table:", err)
+	}
+}
+
+func newSQLite(dbPath string) *proxyFs {
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		log.Fatal("Failed to open database:", err)
+	}
+	initDB(db)
+
+	return &proxyFs{db: db, dbPath: dbPath}
+}
+
+func (r *proxyFs) OnAdd(ctx context.Context) {
 	ch := r.NewPersistentInode(
 		ctx, &fs.MemRegularFile{
 			Data: []byte("This is log file.\n"),
@@ -43,33 +80,34 @@ func (r *HelloRoot) OnAdd(ctx context.Context) {
 	r.AddChild("log.txt", ch, false)
 }
 
-func (r *HelloRoot) Getattr(ctx context.Context, fh fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
-	out.Mode = 0755	
+func (r *proxyFs) Getattr(ctx context.Context, fh fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
+	out.Mode = 0755
 	return 0
 }
 
-func (r *HelloRoot) Create(ctx context.Context, name string, flags uint32, mode uint32, out *fuse.EntryOut) (*fs.Inode, fs.FileHandle, uint32, syscall.Errno) {
-	fd := &fs.MemRegularFile{
-		Data: []byte{},
-		Attr: fuse.Attr{Mode: mode},
+func (r *proxyFs) Create(ctx context.Context, name string, flags uint32, mode uint32, out *fuse.EntryOut) (*fs.Inode, fs.FileHandle, uint32, syscall.Errno) {
+	_, err := r.db.Exec("INSERT INTO files (name, data, mode) VALUES (?, ?, ?) ON CONFLICT(name) DO UPDATE SET data = ?, mode = ?",
+		name, []byte{}, mode, []byte{}, mode)
+	if err != nil {
+		r.logToFile(name + " failed create: " + err.Error())
+		return nil, nil, 0, syscall.EIO
 	}
-	Inode := r.NewPersistentInode(ctx, fd, fs.StableAttr{Ino: 3})
-	r.AddChild(name, Inode, false)
 
-	r.logToFile(name + " create \n")
-	return Inode, fd, 0, 0
+	stable := fs.StableAttr{Ino: uint64(len(r.Children()) + 2), Mode: fuse.S_IFREG}
+	Inode := r.NewPersistentInode(ctx, &SQLiteFile{db: r.db, name: name}, stable)
+	r.AddChild(name, Inode, false)
+	r.logToFile(name + " created")
+	return Inode, nil, 0, 0
 }
 
-
-
-func (r *HelloRoot) Opendir(ctx context.Context) syscall.Errno {
+func (r *proxyFs) Opendir(ctx context.Context) syscall.Errno {
+	r.logToFile("Opendir called")
 	return 0
 }
 
-var _ = (fs.NodeGetattrer)((*HelloRoot)(nil))	
-var _ = (fs.NodeOnAdder)((*HelloRoot)(nil))
-var _ = (fs.NodeCreater)((*HelloRoot)(nil))
-var _ = (fs.NodeOpendirer)((*HelloRoot)(nil))
+var _ = (fs.NodeGetattrer)((*proxyFs)(nil))
+var _ = (fs.NodeCreater)((*proxyFs)(nil))
+var _ = (fs.NodeOpendirer)((*proxyFs)(nil))
 
 func main() {
 	debug := flag.Bool("debug", false, "print debug data")
@@ -77,11 +115,16 @@ func main() {
 	if len(flag.Args()) < 1 {
 		log.Fatal("Usage:\n  hello MOUNTPOINT")
 	}
+
+	dbPath := "fuse.db"
+	fsRoot := newSQLite(dbPath)
 	opts := &fs.Options{}
 	opts.Debug = *debug
-	server, err := fs.Mount(flag.Arg(0), &HelloRoot{}, opts)
+	server, err := fs.Mount(flag.Arg(0), fsRoot, opts)
 	if err != nil {
 		log.Fatalf("Mount fail: %v\n", err)
 	}
-	server.Wait()
+
+
+	server.Wait() 
 }
